eebd3d51a2e948e543d10f9eaa9febd7b3e727da
Alin Flo <floare.alin.fa@gmail.com>
2017-04-18 14:57:19 +0300

    Create basic integration_test for user

---
+++ oca/tests/integration_test_user.py
@@ -0,0 +1,52 @@
+# vim:  set fileencoding=utf8
+# encoding:utf8
+#
+# Author: Alin Floare <alin.floare@databus.systems>
+
+import os
+import unittest
+
+import oca
+from oca.exceptions import OpenNebulaException
+
+
+@unittest.skipUnless(os.environ.get('OCA_INT_TESTS', False),
+                     "Skipping integration tests")
+class IntTestUser(unittest.TestCase):
+    def setUp(self):
+        try:
+            del os.environ["ONE_AUTH"]
+        except KeyError:
+            pass
+
+        self.c = oca.Client(os.environ['OCA_INT_TESTS_ONE_AUTH'], os.environ['OCA_INT_TESTS_ONE_XMLRPC'])
+
+    def tearDown(self):
+        print("teardown")
+        users = oca.UserPool(self.c)
+        users.info()
+        for user in users:
+            if user.name.startswith('inttest_user_'):
+                user.delete()
+
+    def test_allocate(self):
+        user = oca.User.allocate(self.c, 'inttest_user_1', 'inttest_user_1')
+        user = oca.User.allocate(self.c, 'inttest_user_2', 'inttest_user_2')
+        user = oca.User.allocate(self.c, 'inttest_user_3', 'inttest_user_3')
+        user = oca.User.allocate(self.c, 'inttest_user_4', 'inttest_user_4')
+
+    def test_allocate_with_same_name(self):
+        with self.assertRaises(OpenNebulaException):
+            user = oca.User.allocate(self.c, 'inttest_user_2', 'inttest_user_2')
+            user = oca.User.allocate(self.c, 'inttest_user_2', 'inttest_user_2')
+
+    def test_delete(self):
+        users = oca.UserPool(self.c)
+        users.info()
+        for user in users:
+            if user.name.startswith('inttest_user_'):
+                user.delete()
+
+
+if __name__ == "__main__":
+    unittest.main()
