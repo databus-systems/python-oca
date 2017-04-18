e5c0f3d72576b01674525edc589e8d9fa5a7a0e5
Alin Flo <floare.alin.fa@gmail.com>
2017-04-18 11:58:49 +0300

    Create allocate and delete tests for host

--- oca/tests/integration_test_host.py
+++ oca/tests/integration_test_host.py
@@ -30,14 +30,18 @@ class IntTestHost(unittest.TestCase):
                 host.delete()
 
     def test_allocate(self):
-        host = oca.Host.allocate(self.c, '<HOST><NAME>inttest_host_1</NAME><IM_MAD>im_dummy</IM_MAD<VM_MAD>vmm_dummy</VM_MAD><TM_MAD>tm_dummy</TM_MAD></HOST>')
-        host = oca.Host.allocate(self.c, '<HOST><NAME>inttest_host_2</NAME><IM_MAD>im_dummy</IM_MAD<VM_MAD>vmm_dummy</VM_MAD><TM_MAD>tm_dummy</TM_MAD></HOST>')
-        host = oca.Host.allocate(self.c, '<HOST><NAME>inttest_host_3</NAME><IM_MAD>im_dummy</IM_MAD<VM_MAD>vmm_dummy</VM_MAD><TM_MAD>tm_dummy</TM_MAD></HOST>')
-        host = oca.Host.allocate(self.c, '<HOST><NAME>inttest_host_4</NAME><IM_MAD>im_dummy</IM_MAD<VM_MAD>vmm_dummy</VM_MAD><TM_MAD>tm_dummy</TM_MAD></HOST>')
+        host = oca.Host.allocate(self.c, 'inttest_host_1', 'im_dummy', 'vmm_dummy', 0)
+        host = oca.Host.allocate(self.c, 'inttest_host_2', 'im_dummy', 'vmm_dummy', 0)
+        host = oca.Host.allocate(self.c, 'inttest_host_3', 'im_dummy', 'vmm_dummy', 0)
+        host = oca.Host.allocate(self.c, 'inttest_host_4', 'im_dummy', 'vmm_dummy', 0)
 
-    def test_allocate_with_same_name(self):
-        with self.assertRaises(OpenNebulaException):
-            host = oca.Host.allocate(self.c, '<HOST><NAME>inttest_host_1</NAME><IM_MAD>im_dummy</IM_MAD<VM_MAD>vmm_dummy</VM_MAD><TM_MAD>tm_dummy</TM_MAD></HOST>')
+    # def test_allocate_with_same_name(self):
+    #     with self.assertRaises(OpenNebulaException):
+    #         host = oca.Host.allocate(self.c, '<HOST><NAME>inttest_host_1</NAME><IM_MAD>im_dummy</IM_MAD<VM_MAD>vmm_dummy</VM_MAD><TM_MAD>tm_dummy</TM_MAD></HOST>')
+
+    # def test_instantiate(self):
+    #     host = oca.Host(open(os.path.join(os.path.dirname(oca.__file__), 'tests/fixtures/host.xml')).read(), self.c)
+    #     host.allocate()
 
     def test_delete(self):
         hosts = oca.HostPool(self.c)
